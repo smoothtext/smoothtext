4ebde248c78a6289fae033fccccd02b86f428a87
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2025-01-14 23:17:33 +0000

    Refactoring.

--- src/smoothtext/__init__.py
+++ src/smoothtext/__init__.py
@@ -10,7 +10,7 @@ from .formula import ReadabilityFormula
 from .language import Language
 from .smoothtext import SmoothText
 
-__version__ = (0, 0, 12)
+__version__ = (0, 0, 13)
 
 for attribute in dir(ReadabilityFormula):
     if callable(getattr(ReadabilityFormula, attribute)):
